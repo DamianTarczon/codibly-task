87586f355a9eaf52de9c3ea99d4fd15749f44b9f
Damian <d.tarczon@mits.pl>
2024-03-18 10:12:29 +0100

    tailwind init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# codibly-task
+To start project run in terminal:
+1. npm i
+2. npm run start
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-    <div>
+    <div className="flex justify-center">
         cos
     </div>
   );
